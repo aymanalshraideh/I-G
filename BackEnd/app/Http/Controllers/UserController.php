c7c0cb2d70166c2f937c454684632f665a6b2c6c
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-11-28 23:41:21 +0300

    create api for login, register and login and register page in react

---
+++ BackEnd/app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user=new User();
+
+        $user->firstname=$request->firstname;
+        $user->lastname=$request->lastname;
+        $user->password=Hash::make($request->password);
+        $user->email=$request->email;
+        $user->role=$request->role;
+        $user->status=$request->status;
+
+        $user->save();
+        return response()->json(['status'=>200 , 'user'=>$user]);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public  function login(Request $request) {
+        $user = User::where('user_email',$request->user_email)->first();
+ 
+ 
+         $error = "ERROR!";
+ 
+        if (!$user || !Hash::check($request->user_password, $user->user_password)){
+             return $error;
+        }
+ 
+        return $user;
+     }
+ 
+
+
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- reactfrontend/src/components/auth/Register.js
+++ reactfrontend/src/components/auth/Register.js
@@ -1,51 +1,115 @@
-import React from'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+function Register() {
+  const [data, setFormValue] = useState({ firstname: '', lastname: '', email: '', password: '',role: '',status:''});
 
-const Register =()=>{
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  window.axios = require("axios");
+  const Submit = (event) => {
+    event.preventDefault();
+    const api = {
+      firstname: data.firstname,
+      lastname: data.lastname,
+      email: data.email,
+      password: data.password,
+      role:data.role,
+      status: 0
+
+    };
+    console.log(api);
+    // axios.post("http://127.0.0.1:8000/api/adduser", api);
+  };
+
+  // get data
+
+  const valueHandler = (event) => {
+    setFormValue({ ...data, [event.target.name]: event.target.value });
+  };
 
 
-    return (
-        <>
+
+
+  return (
+    <>
+
+      <div className="px-4 py-5 px-md-2 text-center text-lg-start " style={{ backgroundColor: "hsl(0, 0%, 96%)" }}>
         <div className="container">
-        <div className="row">
-        <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">
-      Email address
-    </label>
-    <input
-      type="email"
-      className="form-control"
-      id="exampleInputEmail1"
-      aria-describedby="emailHelp"
-    />
-    
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">
-      Password
-    </label>
-    <input
-      type="password"
-      className="form-control"
-      id="exampleInputPassword1"
-    />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">
-      Password
-    </label>
-    <input
-      type="password"
-      className="form-control"
-      id="exampleInputPassword1"
-    />
-  </div>
-  <button type="submit" className="btn btn-primary">
-    Submit
-  </button>
-</form>
-</div></div>
-        </>
-    )
+          <div className="row gx-lg-5 justify-content-center align-items-center">
+
+
+            <div className="col-lg-6 mb-5 mb-lg-0 ">
+              <div className="card">
+                <div className="card-body py-1 px-md-5">
+                  
+
+                  <form className="mt-5" onSubmit={Submit}  >
+                    {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <label>First Name</label>
+                          <input type="text" id="form3Example1" className="form-control" name='firstname' placeholder='First name' onChange={valueHandler} />
+                          {/* <label className="form-label" for="form3Example1">First name</label> */}
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <label>Last Name</label>
+                          <input type="text" id="form3Example2" className="form-control" name="lastname" placeholder='Last name' onChange={valueHandler} />
+                          {/* <label className="form-label" for="form3Example2">Last name</label> */}
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* <!-- Email input --> */}
+                    <div className="form-outline mb-4">
+                      <label>Email</label>
+                      <input type="email" id="form3Example3" className="form-control" name="email" placeholder='Email address' onChange={valueHandler} />
+                      {/* <label className="form-label" for="form3Example3">Email address</label> */}
+                    </div>
+
+                    {/* <!-- Password input --> */}
+                    <div className="form-outline mb-4">
+                      <label>Password</label>
+                      <input type="password" id="form3Example4" className="form-control" name="password" placeholder='Password' onChange={valueHandler} />
+                      {/* <label className="form-label" for="form3Example4">Password</label> */}
+                    </div>
+                    {/* <!-- select type of user --> */}
+                    <div className="form-outline mb-4">
+                      <label >Sign up as:</label>
+                      <select className="form-select" name='role' onChange={valueHandler} aria-label="Default select example">
+                      <option value={''} selected>Open this select menu</option>
+                        <option value={'admin'}>Admin</option>
+                        <option value={'user'}>User</option>
+
+                      </select>
+                    </div>
+                    {/* <!-- status --> */}
+                    
+                    {/* <!-- Submit button --> */}
+                    <button type="submit" className="btn btn-primary btn-block mb-4">
+                      Sign up
+                    </button>
+
+                    {/* <!-- Register buttons --> */}
+                    <div className="text-center">
+
+                      {/* <button type="submit" className="btn btn-primary btn-block mb-4" >
+                        Login
+                      </button> */}
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </section> */}
+    </>
+  )
 }
-export default Register;
+
+export default Register
